1a1ed4df47a09f38a239f823ff206d155ff244ec
mrrh-dev <sab.rejwan11@gmail.com>
2026-07-05 12:19:32 +0600

    include a real life example

--- JS/Asynchronous P/promise.js
+++ JS/Asynchronous P/promise.js
@@ -1,4 +1,4 @@
-let succes = false;
+/* let succes = false;
 const fp = new Promise((resolve,reject) => {
     // doing some async stuff
     if(succes)
@@ -15,4 +15,33 @@ fp
  .catch((err) =>{
     console.log(err)
  }
-)
+) */
+
+ const takeorder = (customer) =>{
+    return new Promise((resolve,reject) =>{
+        console.log('take order for customer')
+        resolve(customer)
+    })
+ }
+
+ const processorder = (customer)=>{
+    return new Promise((resolve,reject)=>{
+        console.log('processing the order')
+
+        setTimeout(()=>{
+            console.log('cooking compeleted')
+            console.log('order proccesd for customer')
+            resolve(customer)
+        },3000)
+        console.log('print sync stuff')
+    })
+ }
+
+ const completeorder = (customer) => {
+    console.log('comjpleted order for customer')
+ }
+
+takeorder('customer')
+    .then(processorder)
+    .then(completeorder)
+
