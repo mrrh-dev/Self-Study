230215349b3d277c4785613f5f3b1224c465883e
mrrh-dev <sab.rejwan11@gmail.com>
2026-07-07 00:45:00 +0600

    add some method of promise

--- JS/Asynchronous P/promise.js
+++ JS/Asynchronous P/promise.js
@@ -9,20 +9,20 @@ const fp = new Promise((resolve,reject) => {
 })
 
 fp
- .then((res)=> {
+ .then((res) =>{
     console.log(res)
  })
- .catch((err) =>{
+ .catch((err)=>{
     console.log(err)
- }
-) */
+ }) */
 
- const takeorder = (customer) =>{
+
+  /* const takeorder = (customer) =>{
     return new Promise((resolve,reject) =>{
         console.log('take order for customer')
         resolve(customer)
     })
- }
+ } 
 
  const processorder = (customer)=>{
     return new Promise((resolve,reject)=>{
@@ -44,4 +44,43 @@ fp
 takeorder('customer')
     .then(processorder)
     .then(completeorder)
+    .catch((error) => console.log(error)); 
 
+
+console.log('sync stuff') */
+    
+
+// we can also write above function in more simple way.like -> 
+/* const takeorder = (customer) =>{
+        console.log('take order for customer')
+        return Promise.resolve(customer)
+ } */
+
+/* const promise1 = Promise.resolve('promise 1 resolved')
+
+const promise2 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('promise 2 resolved')
+    },2000)
+})
+
+Promise.all([promise1,promise2]).then((res)=>{
+    console.log(res);
+}) */
+ 
+const promise1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('promise 1 resolved')
+    },5000)
+})
+
+const promise2 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('promise 2 resolved')
+    },2000)
+})
+
+
+Promise.race([promise1,promise2]).then((res)=>{
+    console.log(res);
+})
